1b24f0acc7043ed0f18ced88b2dd94177116294b
Valentin <margreiter.valentin@gmail.com>
2021-02-04 15:25:30 +0100

    prepare landing page
    for feature overview purposes

---
+++ src/pages/Landing/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import "./react-router";
+function App() {
+
+ return (
+
+<div className="container">
+    <button href='./pages/comment-section/index.js' className = "section" title="Comment Section" id="comment-section">
+        Comment Section
+    </button>
+
+
+</div>
+
+  );
+  }
+
+export default App;
